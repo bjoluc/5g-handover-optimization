d893c76070746fd2b92c1de3d6bd3a47b0d541d7
bjoluc <mail@bjoluc.de>
2025-03-06 19:09:41 +0100

    Fix upstream simulation scenarios

--- simulation/simulator/environment/scenarios.py
+++ simulation/simulator/environment/scenarios.py
@@ -7,10 +7,11 @@ class MComSmall(BaseEnvironment):
     def __init__(self, **kwargs):
         super().__init__(
             stations=[
-                BaseStation(bs_id, position)
-                for bs_id, position in enumerate([(110, 130), (65, 80), (120, 30)])
+                BaseStation((110, 130)),
+                BaseStation((65, 80)),
+                BaseStation((120, 30)),
             ],
-            ues=[UserEquipment(ue_id) for ue_id in range(5)],
+            ues=[UserEquipment() for _ in range(5)],
             size=(200, 200),
             **kwargs
         )
@@ -20,20 +21,15 @@ class MComMedium(BaseEnvironment):
     def __init__(self, **kwargs):
         super().__init__(
             stations=[
-                BaseStation(bs_id, position)
-                for bs_id, position in enumerate(
-                    [
-                        (95, 240),
-                        (100, 140),
-                        (105, 60),
-                        (35, 200),
-                        (40, 95),
-                        (165, 220),
-                        (160, 110),
-                    ]
-                )
+                BaseStation((95, 240)),
+                BaseStation((100, 140)),
+                BaseStation((105, 60)),
+                BaseStation((35, 200)),
+                BaseStation((40, 95)),
+                BaseStation((165, 220)),
+                BaseStation((160, 110)),
             ],
-            ues=[UserEquipment(ue_id) for ue_id in range(15)],
+            ues=[UserEquipment() for _ in range(15)],
             size=(200, 300),
             **kwargs
         )
@@ -43,26 +39,21 @@ class MComLarge(BaseEnvironment):
     def __init__(self, **kwargs):
         super().__init__(
             stations=[
-                BaseStation(bs_id, position)
-                for bs_id, position in enumerate(
-                    [
-                        (50, 100),
-                        (60, 210),
-                        (90, 60),
-                        (120, 130),
-                        (130, 215),
-                        (140, 190),
-                        (160, 70),
-                        (200, 250),
-                        (210, 135),
-                        (230, 70),
-                        (250, 240),
-                        (255, 170),
-                        (265, 50),
-                    ]
-                )
+                BaseStation((50, 100)),
+                BaseStation((60, 210)),
+                BaseStation((90, 60)),
+                BaseStation((120, 130)),
+                BaseStation((130, 215)),
+                BaseStation((140, 190)),
+                BaseStation((160, 70)),
+                BaseStation((200, 250)),
+                BaseStation((210, 135)),
+                BaseStation((230, 70)),
+                BaseStation((250, 240)),
+                BaseStation((255, 170)),
+                BaseStation((265, 50)),
             ],
-            ues=[UserEquipment(ue_id) for ue_id in range(30)],
+            ues=[UserEquipment() for _ in range(30)],
             size=(300, 300),
             **kwargs
         )
